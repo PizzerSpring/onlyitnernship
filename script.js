b1976e824741a6c1c4632fd3dfe8ef6193ac2eff
PizzerSpring <vladi-slav96@mail.ru>
2023-09-12 19:02:28 +0700

    starting textContent

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
+const btns = document.querySelectorAll('.btn');
+
+const arr = ['Улица', 'Мост', 'Завод', 'Школа', 'Сад', 'Парк', 'Магазин'];
 
 
-btn.addEventListener('click', (e) => {
-    btn.classList.toggle('active');
+btn1.addEventListener('click', (e) => {
+    btn1.classList.toggle('active');
     e.target.textContent === '+' ? e.target.textContent = '- Стадион' : e.target.textContent = '+';
 })
+
+console.log(btns);
+
+for(let i = 0; i < btns.length; i++ ) {
+    btns[i].addEventListener('click', (e) => {
+        btns[i].classList.toggle('active');
+        e.target.textContent === '+' ? e.target.textContent = `- ${arr[i]}` : e.target.textContent = '+';
+    })
+}
