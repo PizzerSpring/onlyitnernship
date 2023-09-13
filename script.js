fc877104ace289e072d932c693142a6700c426fb
PizzerSpring <vladi-slav96@mail.ru>
2023-09-13 20:19:09 +0700

    starting change colors

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const btn1 = document.querySelector(".btn1");
+//const btn1 = document.querySelector(".btn1");
 const btns = document.querySelectorAll('.btn');
 
-const arr = ['Улица', 'Мост', 'Завод', 'Школа', 'Сад', 'Парк', 'Магазин', 'Въезд', 'Здание', 'Река'];
+const city = ['Улица', 'Мост', 'Завод', 'Школа', 'Сад', 'Парк', 'Магазин', 'Въезд', 'Здание', 'Река'];
 
-
+const green = '#6ABF4B';
+const blue = '#3877EE';
 /*btn1.addEventListener('click', (e) => {
     btn1.classList.toggle('active');
     e.target.textContent === '+' ? e.target.textContent = '- Стадион' : e.target.textContent = '+';
@@ -14,6 +15,6 @@ console.log(btns);
 for(let i = 0; i < btns.length; i++ ) {
     btns[i].addEventListener('click', (e) => {
         btns[i].classList.toggle('active');
-        e.target.textContent === '+' ? e.target.textContent = `- ${arr[i]}` : e.target.textContent = '+';
+        e.target.textContent === '+' ? e.target.textContent = `- ${city[i]}` : e.target.textContent = '+';
     })
 }
