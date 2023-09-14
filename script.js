7ef55e8bf052f218dcf812d415b954c8dc83257e
PizzerSpring <vladi-slav96@mail.ru>
2023-09-15 01:41:21 +0700

    button colors

--- script.js
+++ script.js
@@ -4,13 +4,12 @@ const btns = document.querySelectorAll('.btn');
 const city = ['Улица', 'Мост', 'Завод', 'Школа', 'Сад', 'Парк', 'Магазин', 'Въезд', 'Здание', 'Река'];
 
 const green = '#6ABF4B';
-const blue = '#3877EE';
-/*btn1.addEventListener('click', (e) => {
-    btn1.classList.toggle('active');
-    e.target.textContent === '+' ? e.target.textContent = '- Стадион' : e.target.textContent = '+';
-})*/
 
 console.log(btns);
+
+for(let i = 0; i < btns.length; i = i + 3) {
+    btns[i].style.backgroundColor = green;
+}
 
 for(let i = 0; i < btns.length; i++ ) {
     btns[i].addEventListener('click', (e) => {
