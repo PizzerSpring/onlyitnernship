b61994a6c6836c9076876fbcfe1dd5f0bb927270
PizzerSpring <vladi-slav96@mail.ru>
2023-09-17 13:00:54 +0700

    button active

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //const btn1 = document.querySelector(".btn1");
 const btns = document.querySelectorAll('.btn');
+const plus = document.querySelector('.plus');
+const text = document.querySelector('.text');
 
-const city = ['Улица', 'Мост', 'Завод', 'Школа', 'Сад', 'Парк', 'Магазин', 'Въезд', 'Здание', 'Река'];
+const city = ['Стадион', 'Мост', 'Завод', 'Школа', 'Сад', 'Парк', 'Магазин', 'Въезд', 'Здание', 'Река'];
 
 const green = '#6ABF4B';
 
@@ -14,6 +16,7 @@ for(let i = 0; i < btns.length; i = i + 3) {
 for(let i = 0; i < btns.length; i++ ) {
     btns[i].addEventListener('click', (e) => {
         btns[i].classList.toggle('active');
-        e.target.textContent === '+' ? e.target.textContent = `- ${city[i]}` : e.target.textContent = '+';
+        plus.classList.toggle('plus-active');
+        text.textContent === '' ?  text.textContent = `${city[i]}` : text.textContent = '';
     })
 }
