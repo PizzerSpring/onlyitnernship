15589b0b3bc4c12570e43612b8a0f58aed8f0a99
PizzerSpring <vladi-slav96@mail.ru>
2023-09-17 14:04:43 +0700

    refactor button position

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //const btn1 = document.querySelector(".btn1");
 const btns = document.querySelectorAll('.btn');
-const plus = document.querySelector('.plus');
-const text = document.querySelector('.text');
 
 const city = ['Стадион', 'Мост', 'Завод', 'Школа', 'Сад', 'Парк', 'Магазин', 'Въезд', 'Здание', 'Река'];
+
+const t = ['35%', '15%', '20%', '40%', '40%', '60%', '65%', '65%', '70%', '79%'];
+const l = ['60%', '70%', '30%', '35%', '73%', '56%', '67%', '24%', '83%', '58%'];
 
 const green = '#6ABF4B';
 
@@ -11,6 +12,11 @@ console.log(btns);
 
 for(let i = 0; i < btns.length; i = i + 3) {
     btns[i].style.backgroundColor = green;
+}
+
+for(let i = 0; i < btns.length; i++) {
+    btns[i].style.top = t[i];
+    btns[i].style.left = l[i];
 }
 
 for(let i = 0; i < btns.length; i++ ) {
