f0479b99b4e12448455ad7f3eb6a14343319eff5
PizzerSpring <vladi-slav96@mail.ru>
2023-09-17 15:49:18 +0700

    refactor directory

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-//const btn1 = document.querySelector(".btn1");
+// Получаем все кнопки
 const btns = document.querySelectorAll('.btn');
-
-const city = ['Стадион', 'Мост', 'Завод', 'Школа', 'Сад', 'Парк', 'Магазин', 'Въезд', 'Здание', 'Река'];
-
+// Храним надписи
+const city = ['Школа', 'Мост', 'Завод', 'Стадион', 'Сад', 'Парк', 'Магазин', 'Въезд', 'Здание', 'Река'];
+// Храним расположение конопок
 const t = ['35%', '15%', '20%', '40%', '40%', '60%', '65%', '65%', '70%', '79%'];
 const l = ['60%', '70%', '30%', '35%', '73%', '56%', '67%', '24%', '83%', '58%'];
 
 const green = '#6ABF4B';
-
-console.log(btns);
-
-for(let i = 0; i < btns.length; i = i + 3) {
+// Красим кнопки разными цветами
+for(let i = 0; i < btns.length; i += 3) {
     btns[i].style.backgroundColor = green;
 }
-
+// Размещаем кнопки на странице
 for(let i = 0; i < btns.length; i++) {
     btns[i].style.top = t[i];
     btns[i].style.left = l[i];
 }
-
+// Основной функционал активности кнопок
 for(let i = 0; i < btns.length; i++ ) {
     btns[i].addEventListener('click', (e) => {
         btns[i].classList.toggle('active');
